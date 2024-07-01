5503e8af337e75eeb5072fded6483a9e38b4d58c
milenabeicker <milenakessia87@gmail.com>
2024-07-01 11:39:00 -0300

    .

---
+++ RockPaperScissors/ContenteView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContenteView2.swift
+//  RockPaperScissors
+//
+//  Created by Milena Beicker on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContenteView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContenteView2()
+}
